585c8810ff68e771401a53fdf913f15f4ae05f91
Hamafac <borysenko777@gmail.com>
2022-10-11 19:02:57 +0300

    add restart logic in game part & add .idea in gitignore

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const TelegramApi = require('node-telegram-bot-api')
-
+const { gameOptions, againOptions } = require('./options')
 const token = '5611180970:AAHIblsTXNySzrfQodHf7PcGDNJ0gcHLjeo'
 
 const bot = new TelegramApi(token, { polling: true })
 
 const chats = {}
-
-const gameOptions = {
-   reply_markup: JSON.stringify({
-      inline_keyboard: [
-         [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
-         [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
-         [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }],
-         [{ text: '0', callback_data: '0' }],
-      ]
-   })
-}
 
 
 bot.setMyCommands([
@@ -23,6 +12,12 @@ bot.setMyCommands([
    { command: '/info', description: 'Інформація про користувача' },
    { command: '/game', description: 'Вгадай число' }
 ])
+const startGame = async (chatId) => {
+   await bot.sendMessage(chatId, `♂️ Вгадай число від 0 до 9 ♂️`)
+   const randomNumber = Math.floor(Math.random() * 10)
+   chats[chatId] = randomNumber;
+   await bot.sendMessage(chatId, '♂️Нумо, погнали!♂️', gameOptions)
+}
 
 const start = () => {
    bot.on('message', async msg => {
@@ -36,10 +31,7 @@ const start = () => {
          return bot.sendMessage(chatId, `Тебе звати ♂️${msg.from.first_name} ${msg.from.last_name}♂️`)
       }
       if (text === '/game') {
-         await bot.sendMessage(chatId, `♂️ Вгадай число від 0 до 9 ♂️`)
-         const randomNumber = Math.floor(Math.random() * 10)
-         chats[chatId] = randomNumber;
-         return bot.sendMessage(chatId, '♂️Нумо, погнали!♂️', gameOptions)
+         return startGame(chatId);
       }
       return bot.sendMessage(chatId, `Ти написав мені ♂️${text}♂️`)
    })
@@ -47,11 +39,14 @@ const start = () => {
    bot.on('callback_query', async msg => {
       const data = msg.data;
       const chatId = msg.message.chat.id;
-      bot.sendMessage(chatId, `Ти обрав число ♂️${data}♂️`)
+      if (data === '/again') {
+         return startGame(chatId);
+      }
+      await bot.sendMessage(chatId, `Ти обрав число ♂️${data}♂️`)
       if (data === chats[chatId]) {
-         return await bot.sendMessage(chatId, `Вітаю, ти вгадав число ♂️${chats[chatId]}♂️`)
+         return await bot.sendMessage(chatId, `Вітаю, ти вгадав число ♂️${chats[chatId]}♂️`, againOptions)
       } else {
-         return bot.sendMessage(chatId, `Нажаль ти не вгадав число, DungeonMaster загадав ♂️${chats[chatId]}♂️`)
+         return bot.sendMessage(chatId, `Нажаль ти не вгадав число, DungeonMaster загадав ♂️${chats[chatId]}♂️`, againOptions)
       }
    })
 }
